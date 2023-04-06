395aad7742b72fcef9b3e5c77f66ceae455d256d
Rio <kyawsoewin.dev@gmail.com>
2023-04-06 15:26:20 +0630

    Implement Riverpod

---
+++ lib/features/provider/post_provider.dart
@@ -0,0 +1,11 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_testing/features/notifier/post_notifier.dart';
+
+import '../service/post_service.dart';
+
+final postServiceProvider = Provider<PostService>((ref) => PostService());
+
+final postNotifierProvider = StateNotifierProvider<PostNotifier, AsyncValue<List<String>>>((ref){
+  PostService postService = ref.read(postServiceProvider);
+  return PostNotifier(postService);
+});
