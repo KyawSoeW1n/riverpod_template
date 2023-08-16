5cd6e2ef7e2a1645ba6d6a48545eedf37667527d
kyawsoewin <minthant12321@gmail.com>
2023-08-16 17:31:12 +0630

    Implement Change Theme

--- lib/features/post/post_screen.dart
+++ lib/features/post/post_screen.dart
@@ -1,12 +1,8 @@
-import 'dart:developer';
-
-import 'package:animated_theme_switcher/animated_theme_switcher.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:pull_to_refresh/pull_to_refresh.dart';
 import 'package:riverpod_testing/features/post/provider/post_provider.dart';
-import 'package:riverpod_testing/features/post/provider/post_refresh_controller_provider.dart';
 import 'package:riverpod_testing/features/post/provider/post_scroll_controller_provider.dart';
 import 'package:riverpod_testing/features/post/widgets/error_handling_widget.dart';
 import 'package:riverpod_testing/widget/common/common_app_bar.dart';
@@ -38,32 +34,13 @@ class PostScreen extends BaseView {
 
   @override
   Widget body(BuildContext context, WidgetRef ref) {
-    final refreshController = ref.watch(postRefreshControllerProvider);
+    final refreshController = RefreshController();
     final scrollController = ref.watch(postScrollControllerProvider);
     final postProvider = ref.watch(postNotifierProvider);
 
     final getFavouritePostsProvider = ref.watch(favouritePostsStreamProvider);
     return Column(
       children: [
-        // ThemeSwitcher.switcher(
-        //   clipper: const ThemeSwitcherBoxClipper(),
-        //   builder: (context, switcher) {
-        //     return OutlinedButton(
-        //       child: const Text('Box Animation'),
-        //       onPressed: () {
-        //         log(":::::: ${ThemeModelInheritedNotifier.of(context).theme.brightness}");
-        //         // switcher.changeTheme(
-        //         //   theme: ThemeModelInheritedNotifier.of(context)
-        //         //               .theme
-        //         //               .brightness ==
-        //         //           Brightness.light
-        //         //       ? ThemeData.light()
-        //         //       : ThemeData.dark(),
-        //         // );
-        //       },
-        //     );
-        //   },
-        // ),
         getFavouritePostsProvider.when(
           loading: () => const Center(
             child: CircularProgressIndicator(
@@ -85,43 +62,38 @@ class PostScreen extends BaseView {
           },
         ),
         Expanded(
-          child: Consumer(
-            builder: (context, ref, _) {
-              return SmartRefresher(
-                controller: refreshController,
-                onRefresh: () =>
-                    ref.read(postNotifierProvider.notifier).getPostList(),
-                child: postProvider.maybeWhen(
-                  success: (content) => CustomScrollView(
-                    controller: scrollController,
-                    slivers: [
-                      SliverList(
-                        delegate: SliverChildBuilderDelegate(
-                          (BuildContext context, int index) {
-                            return PostItem(
-                              content[index],
-                              ref
-                                  .read(postNotifierProvider.notifier)
-                                  .addFavouritePost,
-                              isFav: getFavouritePostsProvider.value!
-                                  .where((element) =>
-                                      element.id == content[index].id)
-                                  .isNotEmpty,
-                            );
-                          },
-                          childCount:
-                              content.length, // Number of items in the list
-                        ),
-                      ),
-                    ],
+          child: SmartRefresher(
+            controller: refreshController,
+            onRefresh: () =>
+                ref.read(postNotifierProvider.notifier).getPostList(),
+            child: postProvider.maybeWhen(
+              success: (content) => CustomScrollView(
+                controller: scrollController,
+                slivers: [
+                  SliverList(
+                    delegate: SliverChildBuilderDelegate(
+                      (BuildContext context, int index) {
+                        return PostItem(
+                          content[index],
+                          ref
+                              .read(postNotifierProvider.notifier)
+                              .addFavouritePost,
+                          isFav: getFavouritePostsProvider.value!
+                              .where(
+                                  (element) => element.id == content[index].id)
+                              .isNotEmpty,
+                        );
+                      },
+                      childCount: content.length, // Number of items in the list
+                    ),
                   ),
-                  error: (e) => ErrorHandlingWidget(exception: e),
-                  orElse: () => const Center(
-                    child: CircularProgressIndicator(),
-                  ),
-                ),
-              );
-            },
+                ],
+              ),
+              error: (e) => ErrorHandlingWidget(exception: e),
+              orElse: () => const Center(
+                child: CircularProgressIndicator(),
+              ),
+            ),
           ),
         ),
       ],
--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,13 @@
-import 'package:animated_theme_switcher/animated_theme_switcher.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:hive_flutter/adapters.dart';
+import 'package:riverpod_testing/core/enum_collection/theme_type.dart';
 import 'package:riverpod_testing/data_model/cache/favourite_post.dart';
 import 'package:riverpod_testing/resource/theme.dart';
 
 import 'app_constants/app_route_configuration.dart';
 import 'core/theme_provider.dart';
 import 'data_source/local/app_database.dart';
-import 'features/post/post_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -33,28 +32,23 @@ class MyApp extends ConsumerWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final isPlatformDark =
-        WidgetsBinding.instance.window.platformBrightness == Brightness.dark;
-    final initTheme = isPlatformDark ? ThemeData.dark() : ThemeData.light();
-    // final themMode = ref.read(themeController);
-    return ThemeProvider(
-      initTheme: initTheme,
-      builder: (_, myTheme) {
-        return MaterialApp(
-          title: 'Flutter Demo',
-          theme: ThemeData(
-            useMaterial3: true,
-            colorScheme: lightColorScheme,
-          ),
-          darkTheme: ThemeData(
-            useMaterial3: true,
-            colorScheme: darkColorScheme,
-          ),
-          // themeMode: themMode,
-          // home: const MyHomePage(title: 'Flutter Demo Home Page'),
-          home: PostScreen(),
-        );
-      },
+    final themMode = ref.watch(themeController);
+    return MaterialApp.router(
+      title: 'Flutter Demo',
+      themeMode: themMode.theme == ThemeType.light.name
+          ? ThemeMode.light
+          : ThemeMode.dark,
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: lightColorScheme,
+      ),
+      darkTheme: ThemeData(
+        useMaterial3: true,
+        colorScheme: darkColorScheme,
+      ),
+      routerConfig: goRouter,
+      // themeMode: themMode,
+      // home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
     // return MaterialApp(
     //   title: 'Flutter Demo',
