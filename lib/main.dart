60a2ce422153fd99047dae559adc361d43fc2b61
kyawsoewin <minthant12321@gmail.com>
2023-08-17 08:18:57 +0630

    Implement Theme Animation

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,13 @@
+import 'dart:ui';
+
+import 'package:animated_theme_switcher/animated_theme_switcher.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:hive_flutter/adapters.dart';
-import 'package:riverpod_testing/core/enum_collection/theme_type.dart';
+import 'package:riverpod_testing/core/theme/theme_config.dart';
 import 'package:riverpod_testing/data_model/cache/favourite_post.dart';
-import 'package:riverpod_testing/resource/theme.dart';
 
 import 'app_constants/app_route_configuration.dart';
-import 'core/theme_provider.dart';
 import 'data_source/local/app_database.dart';
 
 void main() async {
@@ -32,26 +33,29 @@ class MyApp extends ConsumerWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final themMode = ref.watch(themeController);
-    return MaterialApp.router(
-      title: 'Flutter Demo',
-      themeMode: themMode.theme == ThemeType.light.name
-          ? ThemeMode.light
-          : ThemeMode.dark,
-      theme: ThemeData(
-        useMaterial3: true,
-        colorScheme: lightColorScheme,
-      ),
-      darkTheme: ThemeData(
-        useMaterial3: true,
-        colorScheme: darkColorScheme,
-      ),
-      routerConfig: goRouter,
-      // themeMode: themMode,
-      // home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    final isPlatformDark =
+        PlatformDispatcher.instance.platformBrightness == Brightness.dark;
+    final initTheme = isPlatformDark ? darkTheme : darkTheme;
+    // final themMode = ref.watch(themeController);
+
+    ///User Theme Animation
+    return ThemeProvider(
+      initTheme: initTheme,
+      duration: const Duration(milliseconds: 700),
+      builder: (_, myTheme) {
+        return MaterialApp.router(
+          title: 'Flutter Demo',
+          theme: myTheme,
+          routerConfig: goRouter,
+        );
+      },
     );
-    // return MaterialApp(
+    //
+    // return MaterialApp.router(
     //   title: 'Flutter Demo',
+    //   themeMode: themMode.theme == ThemeType.light.name
+    //       ? ThemeMode.light
+    //       : ThemeMode.dark,
     //   theme: ThemeData(
     //     useMaterial3: true,
     //     colorScheme: lightColorScheme,
@@ -60,9 +64,9 @@ class MyApp extends ConsumerWidget {
     //     useMaterial3: true,
     //     colorScheme: darkColorScheme,
     //   ),
-    //   themeMode: themMode,
+    //   routerConfig: goRouter,
+    //   // themeMode: themMode,
     //   // home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    //   home: PostScreen(),
     // );
   }
 }
