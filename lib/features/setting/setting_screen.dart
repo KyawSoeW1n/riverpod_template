ef458e8a6a560c9090f8816249202a5584af68dc
kyawsoewin <minthant12321@gmail.com>
2023-08-18 12:58:06 +0630

    Implement Change Language

--- lib/features/setting/setting_screen.dart
+++ lib/features/setting/setting_screen.dart
@@ -2,8 +2,13 @@ import 'package:animated_theme_switcher/animated_theme_switcher.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
+import 'package:riverpod_testing/core/locale/locale_provider.dart';
+import 'package:riverpod_testing/core/locale/support_locale.dart';
+import 'package:riverpod_testing/core/resource/app_dimens.dart';
+import 'package:riverpod_testing/widget/common/text_view_widget.dart';
 
 import '../../app_constants/app_routes.dart';
+import '../../core/locale/localization_helper.dart';
 import '../../core/theme/theme_config.dart';
 import '../../core/theme_provider.dart';
 import '../../widget/common/common_app_bar.dart';
@@ -14,6 +19,7 @@ class SettingScreen extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final mode = ref.watch(themeController).theme;
+    final locale = ref.watch(localeProvider);
     return ThemeSwitchingArea(
       child: Scaffold(
         appBar: const CommonAppBar("Setting"),
@@ -26,10 +32,12 @@ class SettingScreen extends ConsumerWidget {
                 builder: (_, switcher, theme) {
                   return Row(
                     children: [
-                      const Expanded(
+                      Expanded(
                         child: Padding(
-                          padding: EdgeInsets.only(left: 16),
-                          child: Text("Dark Mode"),
+                          padding:
+                              const EdgeInsets.only(left: AppDimens.margin16),
+                          child: TextViewWidget(
+                              '${LocalizationHelper.of(context)?.darkMode}'),
                         ),
                       ),
                       IconButton(
@@ -44,6 +52,77 @@ class SettingScreen extends ConsumerWidget {
                   );
                 },
               ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: AppDimens.margin16,
+              ),
+              child: Row(
+                children: [
+                  const Expanded(
+                    flex: 1,
+                    child:  TextViewWidget('Language'),
+                  ),
+                  Expanded(
+                    flex: 2,
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: RadioListTile<String>(
+                            title: const TextViewWidget('Eng'),
+                            value: locale.languageCode,
+                            groupValue: SupportedLocale.en.name,
+                            onChanged: (value) {
+                              ref
+                                  .read(localeProvider.notifier)
+                                  .changeLanguage(SupportedLocale.en);
+                            },
+                          ),
+                        ),
+                        Expanded(
+                          child: RadioListTile<String>(
+                            title: const TextViewWidget('MY'),
+                            value: locale.languageCode,
+                            groupValue: SupportedLocale.fr.name,
+                            onChanged: (value) {
+                              ref
+                                  .read(localeProvider.notifier)
+                                  .changeLanguage(SupportedLocale.fr);
+                            },
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+              // child: Row(
+              //   children: [
+              //     Expanded(
+              //       child: ElevatedButton(
+              //         onPressed: () {
+              //           ref
+              //               .read(localeProvider.notifier)
+              //               .changeLanguage(SupportedLocale.en);
+              //         },
+              //         child: TextViewWidget("ENG"),
+              //       ),
+              //     ),
+              //     SizedBox(
+              //       width: AppDimens.margin16,
+              //     ),
+              //     Expanded(
+              //       child: ElevatedButton(
+              //         onPressed: () {
+              //           ref
+              //               .read(localeProvider.notifier)
+              //               .changeLanguage(SupportedLocale.mm);
+              //         },
+              //         child: TextViewWidget("MM"),
+              //       ),
+              //     ),
+              //   ],
+              // ),
             ),
             // SwitchListTile.adaptive(
             //   title: const Text("Dark Mode"),
--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,28 @@
+import 'dart:developer';
 import 'dart:ui';
 
 import 'package:animated_theme_switcher/animated_theme_switcher.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:riverpod_testing/core/theme/theme_config.dart';
 import 'package:riverpod_testing/data_model/cache/favourite_post.dart';
 
 import 'app_constants/app_route_configuration.dart';
+import 'core/locale/locale_provider.dart';
+import 'core/locale/localization_helper.dart';
 import 'data_source/local/app_database.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Hive.initFlutter();
   Hive.registerAdapter(CachePostAdapter());
+
   final dbService = DatabaseService();
   await dbService.initTheme();
   await dbService.initPostBox();
+  await dbService.initLanguageBox();
   runApp(
     ProviderScope(
       overrides: [
@@ -30,15 +36,16 @@ void main() async {
 class MyApp extends ConsumerWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final locale = ref.watch(localeProvider);
     final isPlatformDark =
         PlatformDispatcher.instance.platformBrightness == Brightness.dark;
     final initTheme = isPlatformDark ? darkTheme : darkTheme;
     // final themMode = ref.watch(themeController);
+    log(">>> ${locale.languageCode}");
 
-    ///User Theme Animation
+    ///Use Theme Animation
     return ThemeProvider(
       initTheme: initTheme,
       duration: const Duration(milliseconds: 500),
@@ -46,10 +53,47 @@ class MyApp extends ConsumerWidget {
         return MaterialApp.router(
           title: 'Flutter Demo',
           theme: myTheme,
+          locale: locale,
+          supportedLocales: const [
+            Locale('en', 'US'),
+            Locale('fr', 'FR'),
+          ],
+          localizationsDelegates: const [
+            GlobalCupertinoLocalizations.delegate,
+            GlobalMaterialLocalizations.delegate,
+            GlobalWidgetsLocalizations.delegate,
+            LocalizationHelper.delegate
+          ],
           routerConfig: goRouter,
         );
       },
     );
+    // return Consumer(
+    //   builder: (BuildContext context, WidgetRef ref, Widget? child) {
+    //     return ThemeProvider(
+    //       initTheme: initTheme,
+    //       duration: const Duration(milliseconds: 500),
+    //       builder: (_, myTheme) {
+    //         return MaterialApp.router(
+    //           title: 'Flutter Demo',
+    //           theme: myTheme,
+    //           locale: locale,
+    //           supportedLocales: const [
+    //             Locale('en', 'US'),
+    //             Locale('my', 'my_MM'),
+    //           ],
+    //           localizationsDelegates: const [
+    //             GlobalCupertinoLocalizations.delegate,
+    //             GlobalMaterialLocalizations.delegate,
+    //             GlobalWidgetsLocalizations.delegate,
+    //             LocalizationHelper.delegate
+    //           ],
+    //           routerConfig: goRouter,
+    //         );
+    //       },
+    //     );
+    //   },
+    // );
     //
     // return MaterialApp.router(
     //   title: 'Flutter Demo',
