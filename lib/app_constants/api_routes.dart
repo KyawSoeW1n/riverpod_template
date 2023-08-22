2d3b5a79d9236d6239ccc315862c9399a0570979
kyawsoewin <minthant12321@gmail.com>
2023-08-22 11:41:10 +0630

    Modify Readme

--- lib/app_constants/api_routes.dart
+++ lib/app_constants/api_routes.dart
@@ -1,5 +1,4 @@
 class ApiRoutes {
   static const getPhoto = "photos";
-  static const getPhotoTest = "photoss";
   static const getToDoList = "todos";
 }
--- lib/extension/exception_extension.dart
+++
@@ -1,9 +0,0 @@
-extension FormattedMessage on Exception {
-  String get getMessage {
-    if (toString().startsWith("Exception: ")) {
-      return toString().substring(11);
-    } else {
-      return toString();
-    }
-  }
-}
