bf635f6fc5b2cc1a3267c1cbe4b5cef70f618fe5
kyawsoewin <minthant12321@gmail.com>
2023-11-03 15:01:02 +0630

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -10,6 +10,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 import 'package:riverpod_testing/data_model/cache/cache_post.dart';
+import 'package:riverpod_testing/data_model/response/post_response.dart';
 import 'package:riverpod_testing/data_source/network/posts/post_remote_datasource_impl.dart';
 import 'package:riverpod_testing/domain/fetch_posts/fetch_posts_usecase_impl.dart';
 import 'package:riverpod_testing/mapper/posts_mapper.dart';
@@ -18,24 +19,39 @@ import 'widget_test.mocks.dart';
 
 @GenerateMocks(
   [
+    PostMapper,
     PostRemoteDataSourceImpl,
     FetchPostsUseCaseImpl,
   ],
 )
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
+  final mockPostMapper = MockPostMapper();
   final postsRemoteDataSourceImpl = MockPostRemoteDataSourceImpl();
   final mockFetchPostsUseCaseImpl = MockFetchPostsUseCaseImpl();
 
   final faker = Faker();
-  final expected = [
-    CachePost(
-      faker.lorem.random.integer(5),
-      faker.lorem.sentence(),
-      faker.lorem.random.boolean(),
-    )
+
+  final responseList = [
+    {
+      "userId": faker.lorem.random.integer(5),
+      "id": faker.lorem.random.integer(5),
+      "title": faker.lorem.sentence(),
+      "body": faker.lorem.sentence(),
+    }
   ];
+
   test('Get Post API', () async {
+    final expectedResult = responseList
+        .map((e) => CachePost(PostItemResponse.fromJson(e).id,
+            PostItemResponse.fromJson(e).title, false))
+        .toList();
+    when(mockPostMapper.mapFromResponse(responseList)).thenAnswer(
+      (_) => expectedResult,
+    );
+
+    final expected = mockPostMapper.mapFromResponse(responseList);
+
     when(postsRemoteDataSourceImpl.getPostList()).thenAnswer(
       (_) async => expected,
     );
@@ -43,8 +59,8 @@ void main() {
     mockFetchPostsUseCaseImpl.fetchPostList();
     verify(mockFetchPostsUseCaseImpl.fetchPostList());
 
-    final result = await postsRemoteDataSourceImpl.getPostList();
+    final actualResult = await postsRemoteDataSourceImpl.getPostList();
 
-    expect(result, expected);
+    expect(actualResult, expected);
   });
 }
